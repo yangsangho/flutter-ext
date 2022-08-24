ed8940727ef48d6b0f1e7e8e785dd72538fbed37
yangsangho <yangsangho22@gmail.com>
2022-08-24 15:12:53 +0900

    # EDIT - equtable in model template

--- src/templates/model.template.ts
+++ src/templates/model.template.ts
@@ -4,25 +4,19 @@ export function getModelTemplate(modelName: string): string {
   const pascalCaseModelName = changeCase.pascalCase(modelName);
   const snakeCasePageName = changeCase.snakeCase(modelName);
 
-  return `import 'package:equatable/equatable.dart';
-import 'package:json_annotation/json_annotation.dart';
-import 'package:pawdly_api/src/model/model.dart';
+  return `import 'package:json_annotation/json_annotation.dart';
+
+import '../../model/json_map.dart';
 
 part '${snakeCasePageName}.g.dart';
 
 @JsonSerializable()
-class ${pascalCaseModelName} extends Equatable {
+class ${pascalCaseModelName} {
 
   const ${pascalCaseModelName}();
 
   factory ${pascalCaseModelName}.fromJson(JsonMap json) => _$${pascalCaseModelName}FromJson(json);
   JsonMap toJson() => _$${pascalCaseModelName}ToJson(this);
-
-  @override
-  bool? get stringify => true;
-
-  @override
-  List<Object?> get props => [];
 }
 `;
 }
